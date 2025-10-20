58c07a2c9951a1c18501af1ec3eb7e9d4d6be2c7
Elvis Teodoro Casco Alfaro <ecasco1@gmail.com>
2025-10-20 14:43:57 -0600

    Adding the rest of the functions and creating library

--- process_data/process_data/src/process_data/data_fill_nans.py
+++ process_data/process_data/src/process_data/data_fill_nans.py
@@ -1,5 +1,4 @@
 #d.Fill NaN with the mean value of the column in the columns: height, weight.
-
 def data_fill_nans(file_name):
     train,test=data_remove_nans(file_name)
     train['height'] = train['height'].fillna(train['height'].mean())
