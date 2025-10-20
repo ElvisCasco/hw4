4aef6b7c89f90fdeccf76573f355a3ddde93c2ab
Elvis Teodoro Casco Alfaro <ecasco1@gmail.com>
2025-10-20 13:45:02 -0600

    Adding the rest of the functions and creating library

--- process_data/process_data/src/process_data/data_predict.py
+++ process_data/process_data/src/process_data/data_predict.py
@@ -1,17 +1,28 @@
 #g.& h. Predict the targets for both the train and test sets and add the prediction as a new column 
+def data_logistic_model(file_name):
+     # Get the logistic regression model using training data 
+     train,test=data_binary(file_name)
+     from sklearn.linear_model import LogisticRegression
+     features = [
+         'age', 'height', 'weight', 'aids', 'cirrhosis',
+          'hepatic_failure','immunosuppression', 'leukemia', 
+          'lymphoma', 'solid_tumor_with_metastasis']
+     # Get features and target variables
+     X_train = train[features]
+     y_train = train['diabetes_mellitus']
+     X_test = test[features]
+     y_test = test['diabetes_mellitus']
+     # Fit the model with training data
+     model = LogisticRegression(max_iter=1000)
+     model.fit(X_train, y_train)
+     return X_train,y_train,X_test,y_test,model
 
-def data_train_models(file_name): 
-    train,test=data_binary(file_name)
-    from sklearn.linear_model import LogisticRegression
-    features = ['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure',
-            'immunosuppression', 'leukemia', 'lymphoma', 'solid_tumor_with_metastasis']
-    X_train = train[features]
-    y_train = train['diabetes_mellitus']
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
-    y_proba = model.predict_proba(X_test_std)[:, 1]
-    
-
-    return model
-
-    
+def data_predict(file_name): 
+     train,test=data_binary(file_name)
+     X_train,y_train,X_test,y_test,model=data_logistic_model(file_name)
+     # Predict class labels and probabilities 
+     train['predictions'] = model.predict(X_train)
+     train['predictions_prob'] = model.predict_proba(X_train)[:, 1]
+     test['predictions'] = model.predict(X_test)
+     test['predictions_prob'] = model.predict_proba(X_test)[:, 1]
+     return train,test
---
+++ process_data/process_data/src/process_data/pred_auc_score.py
@@ -0,0 +1,17 @@
+
+def pred_auc_score(file_name):
+     from sklearn.metrics import roc_auc_score, accuracy_score
+     train,test=data_predict(file_name)
+     # Evaluate performance
+     y_train = train['diabetes_mellitus']
+     y_test = test['diabetes_mellitus']
+     train_auc = roc_auc_score(y_train, train['predictions_prob'])
+     #train_accuracy = accuracy_score(y_train, train['predictions'])
+     test_auc = roc_auc_score(y_test, test['predictions_prob'])
+     #test_accuracy = accuracy_score(y_test, train['predictions'])
+     # Print results
+     print(f"\nTrain AUC: {train_auc:.4f}\n")
+     #print(f"Train Accuracy: {train_accuracy:.4f}")
+     print(f"\nTest AUC: {test_auc:.4f}\n")
+     #print(f"\nTest Accuracy: {test_accuracy:.4f}")
+
