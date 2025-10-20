d6cc2620437e99da164ee419eb502bdc550da0d6
majoargote <majoargote@gmail.com>
2025-10-20 15:29:53 +0200

    finished hw4 file

--- hw4.py
+++ hw4.py
@@ -32,17 +32,14 @@ print(count_simba(list))
 import pandas as pd
 
 def get_day_month_year(date_list):
-    days = []
-    months = []
-    years = []
-    for date in date_list:
-        days.append(date.day)
-        months.append(date.month)
-        years.append(date.year)
-        df = pd.DataFrame({'day': days, 'month': months, 'year': years})
-    return df
+    days, months, years = [], [], []
+    for d,m,y in date_list:
+        days.append(int(d))
+        months.append(int(m))
+        years.append(int(y))
+    return pd.DataFrame({'day': days, 'month': months, 'year': years})
 
-dates = [["22","05","1992"],["25","05","1958"],["06","22","1961"],["08","04","1995"]]
+dates = [["22","05","1992"], ["25","05","1958"], ["06","22","1961"], ["08","04","1995"]]
 
 print(get_day_month_year(dates))
 
@@ -54,6 +51,20 @@ print(get_day_month_year(dates))
 # example input: [((41.23,23.5), (41.5, 23.4)), ((52.38, 20.1),(52.3, 17.8))]
 # HINT: You can use geopy.distance in order to compute the distance
 #
+
+from geopy.distance import geodesic
+
+def compute_distance(coord_list):
+    coord_distances = []
+    for coord_pair in coord_list:
+        distance = geodesic(coord_pair[0], coord_pair[1]).km
+        coord_distances.append(distance)
+    return coord_distances
+
+coords = [((41.23,23.5), (41.5, 23.4)), ((52.38, 20.1),(52.3, 17.8))]
+
+print(f"{compute_distance(coords)} km")
+
 
 #################################################
 # 4)
@@ -67,4 +78,15 @@ print(get_day_month_year(dates))
 # the result should be 13
 #
 
+def sum_general_int_list(general_list):
+    total = 0
+    for item in general_list:
+        if isinstance(item, list):
+            total += sum_general_int_list(item)
+        else:
+            total += item
+    return total
 
+sample_list = [[1], 2, 3, [4, [5], [6, 7, [8,9]], 10], 1]
+
+print(sum_general_int_list(sample_list))
