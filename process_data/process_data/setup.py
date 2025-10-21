44c8c4d310e42e5a84b89d8b962791cb1183a747
Elvis Teodoro Casco Alfaro <ecasco1@gmail.com>
2025-10-21 10:22:40 +0200

    Add files via upload

---
+++ process_data/process_data/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_library',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+    author='ElvisCasco',
+    description='A library to process data and apply logistic regression',
+    url='https://github.com/ElvisCasco/process_data',
+)
