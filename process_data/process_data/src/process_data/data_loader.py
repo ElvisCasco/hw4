8c7b92aeb0a703bf14a480e6c135620e247b8b69
Elvis Teodoro Casco Alfaro <ecasco1@gmail.com>
2025-10-20 18:04:00 -0600

    Update data_loader.py

--- process_data/process_data/src/process_data/data_loader.py
+++ process_data/process_data/src/process_data/data_loader.py
@@ -1,7 +1,7 @@
 
 # a. Load the data
 def data_loader(file_name: str) -> pd.DataFrame:
-    import pandas as pd
+    #import pandas as pd
     data = pd.read_csv(file_name)
     print(f"Data loaded successfully. Shape: {data.shape}")
     return data
